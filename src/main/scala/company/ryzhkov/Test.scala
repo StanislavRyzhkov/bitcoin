447ebc9ee986290b4c57842fa67b091073223910
Stanislav Ryzhkov <rvmail@mail.ru>
2022-03-13 14:23:38 +0700

    fixed

--- src/main/scala/company/ryzhkov/Test.scala
+++ src/main/scala/company/ryzhkov/Test.scala
@@ -1,8 +1,24 @@
 package company.ryzhkov
 
 object Test extends App {
-  val x = BigInt(7)
-  val y = BigInt(3)
-  val z = x / y
+  def sign(x: BigInt) =
+    if (x < 0) -1
+    else if (x == 0) 0
+    else 1
+
+  def mod(n: BigInt, d: BigInt): BigInt = {
+    val result = n % d
+    if (sign(result) * sign(d) < 0) result + d
+    else result
+  }
+
+  val x = BigInt("-37732016455592228448156445462498416408852156224504820822700452822953572406827")
+//  val x = BigInt("-11")
+//  println(x)
+  val y = BigInt("115792089237316195423570985008687907853269984665640564039457584007908834671663")
+//  val y = BigInt("2")
+//  println(y)
+  val z = mod(x, y)
+
   println(z)
 }
